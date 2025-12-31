671fe0d1317aa998f73fb505fe19a3d137e109c3
Abinash <abixnash@gmail.com>
2025-12-31 21:46:52 +0530

    layout, Header, Footer, Contact page, projet page with dummy data setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["JetBrains Mono", "monospace"],
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+};
